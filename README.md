9b854aff54d9653fc86362bcdf35176352b5660a
Soulaimane yahya <soulaimaneyh07@gmail.com>
2023-05-25 15:47:28 +0100

    updating web page

---
+++ README.md
@@ -0,0 +1,27 @@
+# soulaimaneyahya.com
+
+Software engineer with experience building web applications with focus on back-end development and APIs. Skilled in PHP, Laravel, MySQL, and other relevant technologies.
+
+## Install dependencies
+```
+npm install
+```
+
+## Run GruntJS
+
+```
+grunt concat-js && grunt concat-css
+```
+
+```
+grunt uglify && grunt cssmin
+```
+
+----- 
+Need helps? Reach me out
+
+> Email: contact@soulaimaneyahya.com
+
+> Linkedin: soulaimane-yahya
+
+All the best :beer:
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,51 @@
-const tooltips = document.querySelectorAll('.tt')
-  tooltips.forEach(t => {
-    new bootstrap.Tooltip(t)
-  })
-  // footer
-  document.querySelector("#currentYear").innerHTML = new Date().getFullYear();
-  // modal
-  let span = document.querySelectorAll('.projectImageModal');
-  span.forEach((i) => {
-    i.addEventListener('click', function(){
-      let id = i.dataset.projectId
-      let project = i.dataset.projectName
-      document.getElementById("project-name").innerHTML = project;
+const homeCli = "cd /about/";
+const aboutCli = "cat soulaimaneyahya.txt";
 
-      let images = JSON.parse(i.dataset.images);
-  
-      let text = ""
-      if (images.length > 0) {
-        for (let j = 0; j < images.length; j++) {
+const homeContent = [
+    "",
+    "Software engineer with experience building web applications with focus on back-end development and APIs. Skilled in PHP, Laravel, MySQL, and other relevant technologies.",
+    "",
+    "Social media and contact:",
+    "<a href='https://soulaimaneyahya.com' target='_blank' class='social-media-link'>> Website</a>",
+    "<a href='https://github.com/soulaimaneyahya' target='_blank' class='social-media-link'>> GitHub</a>",
+    "<a href='https://twitter.com/soulaimaneyahya' target='_blank' class='social-media-link'>> Twitter</a>",
+    "<a href='https://www.linkedin.com/in/soulaimane-yahya' target='_blank' class='social-media-link'>> Linkedin</a>",
+    "<a href='mailto:contact@soulaimaneyahya.com' class='social-media-link'>> Email</a>",
+    "",
+    "Languages and frameworks I have worked with:",
+    "<span class='languages-tech'>▪ PHP</span>",
+    "<span class='languages-tech'>▪ Laravel</span>",
+    "<span class='languages-tech'>▪ Javascript</span>",
+    "<span class='languages-tech'>▪ NodeJS</span>"
+];
 
-          if (j == 0) {
-          text += `
-          <div class="carousel-item active">
-              <img src="`+ window.location.href +`assets/project-${id}/`+ images[j]['path'] +`" alt="...">
-          </div>
-          `
-          } 
-          else {
-          text += `
-          <div class="carousel-item">
-              <img src="`+ window.location.href +`assets/project-${id}/`+ images[j]['path'] +`" alt="...">
-          </div>
-          `
-          }
-        }
-    
-      }
-      else {
-        text = `
-          <div class="carousel-item active">
-          <img src="`+ window.location.href +`assets/images/project-default.png" alt="...">
-          </div>
-        `
-      }
-    
-      document.getElementById("carousel-inner").innerHTML = text;
+const homeCliElement = document.getElementById('home-cli');
+const aboutCliElement = document.getElementById('home-about');
+const homeContentElement = document.getElementById('home-content');
 
-      var buttons = ""
-      if (images.length > 1) {
-      buttons = `
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-      `
-      }
-      document.getElementById("carousel-buttons").innerHTML = buttons
+const typingDelayMultiplier = 10;
 
-    });
-  });
+setTimeout(() => {
+    displayCommand(homeCliElement, '~', homeCli);
+    setTimeout(() => {
+        displayCommand(aboutCliElement, '~/about', aboutCli);
+        typeContent(homeContent, 0);
+    }, 500);
+}, 500);
+
+function displayCommand(element, path, displayText) {
+    const commandText = `<span class="cli-text"><span class="cli-home">soulaimaneyahya@home:<span class="cli-home-path">${path}</span></span>$ ${displayText}</span><br>`;
+    element.innerHTML += commandText;
+}
+
+function typeContent(content, index) {
+    if (index < content.length) {
+        const line = content[index];
+        const delay = (line.length * typingDelayMultiplier) / 2;
+
+        setTimeout(() => {
+            homeContentElement.innerHTML += `<span class="cli-text">${line}</span><br>`;
+            typeContent(content, index + 1);
+        }, delay);
+    }
+}
